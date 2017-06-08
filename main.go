26938941bddd6483493e39e538a479b54cb51efc
Damir <dtimurshin@plazius.ru>
2017-06-08 18:05:12 +0300

    Add very basic http session journal

--- main.go
+++ main.go
@@ -1,65 +1,117 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/elazarl/goproxy"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-
-	"github.com/elazarl/goproxy"
+	"sync"
 )
 
 func main() {
-	var endpointMap EndpointMap
-	server := &http.Server{
-		Addr: ":8080",
-	}
+	storage := NewInMemoryStorage(100, 100)
+	proxy := getProxy(storage)
+	api := getApi(storage)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		log.Fatal(http.ListenAndServe(":8889", proxy))
+	}()
+	go func() {
+		defer wg.Done()
+		log.Fatal(http.ListenAndServe(":8000", api))
+	}()
+	wg.Wait()
+}
+
+func getProxy(storage SessionsStorage) *goproxy.ProxyHttpServer {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
 	proxy.OnRequest().DoFunc(func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-		endpointMap.Register(host(r.RemoteAddr), host(r.Host))
 		return r, nil
 	})
 	proxy.OnResponse().DoFunc(func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
-		r.Header.Set("X-GoProxy", "yxorPoG-X")
+		storage.SaveHttp(HttpSession{
+			request:  ctx.Req,
+			response: r,
+		})
 		return r
 	})
-	log.Fatal(http.ListenAndServe(":8888", proxy))
+	return proxy
 }
 
-type appHandler struct {
-
+func getApi(sessionsStorage SessionsStorage) *mux.Router {
+	r := mux.NewRouter()
+	r.Handle("/http/sessions", GetSessionsHandler{sessionsStorage: sessionsStorage}).Methods("GET")
+	return r
 }
 
-func (ah appHandler) ServeHTTP(w http.ResponseWriter, *http.Request) {
-	w.
+type GetSessionsHandler struct {
+	sessionsStorage SessionsStorage
 }
 
-
-func host(remoteAddr string) Endpoint {
-	return Endpoint{[]EndpointAddress{EndpointAddress{remoteAddr, "host"}}}
+func (handler GetSessionsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	allHttp := handler.sessionsStorage.GetAllHttp()
+	list := make([]HttpSessionListItem, len(allHttp))
+	for i := 0; i < len(allHttp); i++ {
+		request := allHttp[i].request
+		list[i] = HttpSessionListItem{
+			From: request.RemoteAddr,
+			To:   request.Host,
+		}
+	}
+	json, _ := json.Marshal(list)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
 }
 
-type Endpoint struct {
-	Addresses []EndpointAddress
+type HttpSessionListItem struct {
+	From string
+	To   string
 }
 
-type EndpointAddress struct {
-	Address  string
-	AddrType string
+type HttpSession struct {
+	request  *http.Request
+	response *http.Response
+	stats    Stats
 }
 
-type WireRegistry interface {
-	Register(requestEndpoint Endpoint, responseEndpoint Endpoint)
-	RequestEndpoints() []Endpoint
+type Stats struct {
 }
 
-func (m *EndpointMap) Register(requestEndpoint Endpoint, responseEndpoint Endpoint) {
-	*m = append(*m, struct {
-		requestEndpoint  Endpoint
-		responseEndpoint Endpoint
-	}{requestEndpoint, responseEndpoint})
+type SessionsStorage interface {
+	SaveHttp(session HttpSession)
+	GetAllHttp() []HttpSession
 }
 
-type EndpointMap []struct {
-	requestEndpoint  Endpoint
-	responseEndpoint Endpoint
+type InMemoryStorage struct {
+	channel chan HttpSession
+	data    []HttpSession
+}
+
+func NewInMemoryStorage(chanBufferSize int, initialBufferCapacity int) *InMemoryStorage {
+	storage := &InMemoryStorage{
+		channel: make(chan HttpSession, chanBufferSize),
+		data:    make([]HttpSession, 0, initialBufferCapacity),
+	}
+	go func() {
+		for ss := range storage.channel {
+			storage.data = append(storage.data, ss)
+		}
+	}()
+	return storage
+}
+
+func (storage *InMemoryStorage) SaveHttp(session HttpSession) {
+	storage.channel <- session
+	return
+}
+
+func (storage *InMemoryStorage) GetAllHttp() []HttpSession {
+	len := len(storage.data)
+	dataToReturn := make([]HttpSession, len, len)
+	copy(dataToReturn, storage.data)
+	return dataToReturn
 }
